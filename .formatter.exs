9602ae8d711fcd46b0f74ccb25a0039fcc624f6a
Pedro Gutiérrez <pedrogutierrez@mac.com>
2023-12-01 19:41:08 +0100

    Release v0.5.2

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,8 @@
-locals_without_parents = [tag: :*, child: :*, attribute: :*]
+locals_without_parens = [tag: :*, child: :*, attribute: :*]
 
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: locals_without_parents,
+  locals_without_parens: locals_without_parens,
   export: [
     locals_without_parens: locals_without_parens
   ]
